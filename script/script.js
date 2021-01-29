9c0df121829c0fe586e65efba5c55b4c8f558a30
Sergey Fedoseenkov <frodor@smc-sm.ru>
2021-01-29 13:06:10 +0300

    Урок №2, усложненое задание, перемножение элементов массива

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
 
+let num = 266219;
+
+let array = num.toString().split('');
+
+console.log(array);
+
+res = array.reduce(function(a, b){
+  return a * b;
+});
+
+console.log(res);
+
+let resCube = res ** 3;
+
+console.log(resCube);
+
+console.log(resCube.toString().substr(0, 2));
