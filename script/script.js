23c748b6aa6bf91687af2f6f95b5a86366335425
Sergey Fedoseenkov <frodor67@bk.ru>
2021-02-01 19:47:26 +0300

    feat: Добавить условия if, switch-case, array и тернарные операторы

--- script/script.js
+++ script/script.js
@@ -1,18 +1,56 @@
+'use strict';
 
-let num = 266219;
+// Задание 1.
 
-let array = num.toString().split('');
+let lang = prompt('Какой язык Ru ли En? ');
 
-console.log(array);
+const ru = 'Ru';
+const en = 'En';
 
-res = array.reduce(function(a, b){
-  return a * b;
-});
+const nameDayRu = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+const nameDayEn = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'];
 
-console.log(res);
 
-let resCube = res ** 3;
+// Решение через if
+if (lang === 'Ru') {
+  console.log(nameDayRu);
+}else if (lang === 'En') {
+  console.log(nameDayEn);
+}else {
+  console.log('Язык выбран не верно');
+}
 
-console.log(resCube);
+//Решение через switch-case
 
-alert(resCube.toString().substr(0, 2));
+switch (lang) {
+  case 'Ru':
+    console.log(nameDayRu);
+    break;
+  case 'En':
+    console.log(nameDayEn);
+    break;
+  default:
+    console.log('Язык выбран не верно');
+    break;
+}
+
+//Решение через многомерные массивы
+
+const langDayArr = [];
+
+langDayArr[ru] = nameDayRu;
+langDayArr[en] = nameDayEn;
+
+console.log(langDayArr[lang]);
+
+
+//Задание 2.
+
+let namePerson = prompt('Как твое имя?');
+
+let msg = namePerson === 'Артем' ? 'Директор' :
+  namePerson === 'Максим' ? 'Преподаватель' : 'Студент';
+
+console.log(msg);
+
+
