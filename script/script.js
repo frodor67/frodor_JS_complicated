2bc8dd7327c850c75a5ac717607c75f48d95cc2d
Sergey Fedoseenkov <frodor67@bk.ru>
2021-02-02 19:00:20 +0300

    feat: добавить функцию + методы

--- script/script.js
+++ script/script.js
@@ -2,55 +2,25 @@
 
 // Задание 1.
 
-let lang = prompt('Какой язык Ru ли En? ');
+let str = prompt('Введите любый буквы...');
 
-const ru = 'Ru';
-const en = 'En';
+function chekString(str) {
+  if (typeof(str) !== 'string') {
+    console.log('Аргумент не строка!');
+  } else {
+    str = str.trim();
+  }
 
-const nameDayRu = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-const nameDayEn = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'];
-
-
-// Решение через if
-if (lang === 'Ru') {
-  console.log(nameDayRu);
-}else if (lang === 'En') {
-  console.log(nameDayEn);
-}else {
-  console.log('Язык выбран не верно');
+  if (+str.length > 30) {
+    console.log(str.substring(0, 30) + '...');
+  }
 }
 
-//Решение через switch-case
-
-switch (lang) {
-  case 'Ru':
-    console.log(nameDayRu);
-    break;
-  case 'En':
-    console.log(nameDayEn);
-    break;
-  default:
-    console.log('Язык выбран не верно');
-    break;
-}
-
-//Решение через многомерные массивы
-
-const langDayArr = [];
-
-langDayArr[ru] = nameDayRu;
-langDayArr[en] = nameDayEn;
-
-console.log(langDayArr[lang]);
+chekString(str);
 
 
-//Задание 2.
 
-let namePerson = prompt('Как твое имя?');
 
-let msg = namePerson === 'Артем' ? 'Директор' :
-  namePerson === 'Максим' ? 'Преподаватель' : 'Студент';
 
-console.log(msg);
 
 
