1df368d5cbce150141589b21b5a0d9f3db70eacb
Sergey Fedoseenkov <frodor67@bk.ru>
2021-02-08 21:25:42 +0300

    improvement: вывод элементов массива, вычисление простых чисел

--- script/script.js
+++ script/script.js
@@ -1,32 +1,43 @@
 'use strict';
 
-// Задание 1.
+//Задание 1
 
-let str = prompt('Введите любые буквы...');
+let arr = ['123345', '23456', '543456', '46732', '56745', '546754', '34563'];
 
+let getArr = function (arr) {
 
+  for (let i = 0; i < arr.length; i++){
 
-function chekString(str, countChar) {
-  if (isNaN(str)) {    
-    countChar(str);    
-  } else {console.log('Аргумент не строка!');}  
-  }
-
-
-  function countChar(str) {
-    str = str.trim();
-    if (+str.length > 30) {
-      console.log(str.substring(0, 30) + '...');
-  } else {
-    console.log(typeof(str), str);
-  }
-
+    if (arr[i].charAt(0) === '2' || arr[i].charAt(0) === '4') {
+    console.log(arr[i]);
+  } 
 }
+};
+
+getArr(arr);
 
 
 
-chekString(str, countChar);
+//Задание 2
 
+let getPrime = function (n) {
+
+  
+nextNum:  for (let i = 2; i <= n; i++) {
+
+    for (let j = 2; j < i; j++){
+
+      if ( i % j === 0) {
+        continue nextNum;
+      }
+    } 
+    
+    console.log(i + ' простое число, делители этого числа 1 и ' + i);        
+      
+  }
+};
+  
+getPrime(100);
 
 
 
